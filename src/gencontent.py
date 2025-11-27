c25b5c40c8552ece854c6ee6ef5974fb8407e0f8
trinido <trinido@gmail.com>
2025-11-26 21:36:59 -0600

    updated files to match solution files prior to deployment step

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,13 +1,21 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+
 def generate_page(from_path, template_path, dest_path):
-    
-    dest_path = dest_path.replace(".md", ".html")
-    
     print(f" * {from_path} {template_path} -> {dest_path}")
-    
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
     from_file.close()
@@ -29,20 +37,6 @@ def generate_page(from_path, template_path, dest_path):
     to_file = open(dest_path, "w")
     to_file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
-
-    for filename in os.listdir(dir_path_content):
-        from_path = os.path.join(dir_path_content, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
-        
-        print(f" * {from_path} -> {dest_path}")
-        
-        if os.path.isfile(from_path):
-            generate_page(from_path, template_path, dest_path)
-        else:
-            generate_pages_recursive(from_path, template_path, dest_path)    
 
 def extract_title(md):
     lines = md.split("\n")
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import shutil
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
+
 dir_path_static = "./static"
 dir_path_public = "./public"
 dir_path_content = "./content"
@@ -18,12 +19,8 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating page...")
-    generate_pages_recursive(
-        dir_path_content,
-        template_path,
-        dir_path_public,
-    )
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
 main()
